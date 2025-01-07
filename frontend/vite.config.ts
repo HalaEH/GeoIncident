d8c882ece143ae6ab82519b80f652dc9732757d2
Hala <halaemadhelmy@gmail.com>
2025-01-07 18:44:20 +0000

    Add Dockerfiles for frontend and backend with docker-compose configuration
    This commit includes:
    1. Added Dockerfile for the backend with configuration for Flask application
    2. Added Dockerfile for the frontend with configuration for React application
    3. Created docker-compose.yaml to orchestrate frontend and backend containers
    
    Signed-off-by: Hala Abdelrahman <halaemadhelmy@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,16 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+const isDocker = process.env.DOCKER_ENV === 'true';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: isDocker ? 'http://backend:5000' : 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
         ws: true
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
